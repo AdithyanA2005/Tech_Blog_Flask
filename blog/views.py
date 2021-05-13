0d72de201e40cbc6a9d9952c5e3f46b613f0e079
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-13 08:38:55 +0530

    The project is structuralised

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from blog.blog import app
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
